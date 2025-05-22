3174ed7f0760f6b0c6df4ff55f3354f63bc39f56
rajneeshsharma9 <rajneesh@brosa.com>
2025-05-23 00:56:18 +0530

    Handle test case when a single negative number is passed

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -8,11 +8,14 @@ class Calculator
     delimiter = match ? match[1] : ','
     return numbers_string.to_i if !numbers_string.include?(delimiter) && numbers_string.to_i.is_a?(Integer)
 
-    return numbers_string
+    numbers_array = numbers_string
       .split('\n')
       .map {|numbers| numbers.split(delimiter)}
       .flatten
       .map(&:to_i)
-      .sum
+
+    raise ArgumentError, "negative numbers not allowed #{numbers_array.select(&:negative?).join(',')}" if numbers_array.any?(&:negative?)
+
+    return numbers_array.sum
   end
 end
