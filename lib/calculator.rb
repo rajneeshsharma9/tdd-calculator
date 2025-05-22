cb1ef08f38c3c2d9e6d055751905885c2f434fb4
rajneeshsharma9 <rajneesh@brosa.com>
2025-05-23 00:40:14 +0530

    Handle test case when delimter is special character

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -4,12 +4,13 @@ class Calculator
   def add(numbers_string)
     return 0 if numbers_string == ''
 
-    delimiter = ','
+    match = numbers_string.match(%r{//(.)})
+    delimiter = match ? match[1] : ','
     return numbers_string.to_i if !numbers_string.include?(delimiter) && numbers_string.to_i.is_a?(Integer)
 
     return numbers_string
       .split('\n')
-      .map {|numbers| numbers.split(',')}
+      .map {|numbers| numbers.split(delimiter)}
       .flatten
       .map(&:to_i)
       .sum
